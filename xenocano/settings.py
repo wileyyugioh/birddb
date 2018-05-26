6e87d0551f7f1cb89ab77eb21d5ff5181a2fd1dc
Wiley Y <nein@nein.nein>
2018-05-25 18:11:59 -0700

    relocate static pt 2

--- xenocano/settings.py
+++ xenocano/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'birddb.apps.BirddbConfig',
+    'front.apps.FrontConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
